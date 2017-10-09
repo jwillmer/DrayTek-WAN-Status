59f307c6831a93f63fc5795a61f800cf4a277a5f
Jens Willmer <info@jwillmer.de>
2017-10-09 15:07:36 +0200

    Add project files.

---
+++ DrayTek WAN Status/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrayTek_WAN_Status {
+
+    class Program {
+        static void Main(string[] args) {
+            var app = new CommandLineApplication();
+            app.Name = "DrayTek WAN Status";
+            app.Description = "The app collects the WAN status message that is send via UDP from a DrayTek modem to your machine.";
+            app.HelpOption("-?|-h|--help");
+            // var optionHide = app.Option("-hide", "Start application hidden.", CommandOptionType.NoValue);
+            var optionIp = app.Option("-ip <ip>", "The DrayTek router IP. Must be a IP address. Default is 192.168.1.1", CommandOptionType.SingleValue);
+            var optionPort = app.Option("-port <port>", "The DrayTek router port. Must be a number. Default is 514", CommandOptionType.SingleValue);
+
+
+            app.OnExecute(() => {
+                var ip = optionIp.HasValue() ? optionIp.Value() : "192.168.1.1";
+                var port = optionPort.HasValue() ? optionPort.Value() : "514";
+                // var hideWindow = optionHide.HasValue();
+
+                if(IPAddress.TryParse(ip, out var ipAddress) && Int32.TryParse(port, out var portNumber)) {
+                    Run(ipAddress, portNumber);
+                } else {
+                    Console.WriteLine("IP address or port could not be recognized.");
+                }                    
+             
+                return 0;
+            });
+
+
+            app.Execute(args);           
+        }
+
+        static void Run(IPAddress ip, int port) {
+            Console.WriteLine($"Listening for UDP packets from {ip}:{port}");
+            Console.WriteLine();
+
+            using (var client = new UdpClient(port)) {
+
+
+                Task.Factory.StartNew(async () => {
+                    while (true) {
+                        try {
+                            var received = await client.ReceiveAsync();
+                            if (received.RemoteEndPoint.Address.Equals(ip) && received.RemoteEndPoint.Port == port) {
+                                var content = Encoding.ASCII.GetString(received.Buffer, 0, received.Buffer.Length);
+                                var wan = WanStatus.Parse(content);
+
+                                Console.WriteLine($"WAN Connected: {wan.IsConnected}");
+                                Console.WriteLine($"Download:      {wan.DownSpeed} {wan.SpeedUnit}");
+                                Console.WriteLine($"Upload:        {wan.UpSpeed} {wan.SpeedUnit}");
+                                Console.WriteLine($"Timestamp:     {wan.Timestamp.TimeOfDay}");
+
+                                Console.SetCursorPosition(0, Console.CursorTop - 4);
+
+                                WriteToDisk(wan);
+                            }
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                });
+
+
+                while (true) {
+                    Console.ReadLine();
+                }
+
+            }
+        }
+
+        static string FilePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\DrayTek-WAN-Status.csv";
+
+        static void WriteToDisk(WanStatus wan) {
+            if (!File.Exists(FilePath)) {
+                var header = $"Date; Time; Is Connected; Upload Speed ({wan.SpeedUnit}); Download Speed ({wan.SpeedUnit})";
+                File.WriteAllText(FilePath, header + Environment.NewLine);
+            }
+
+            var line = $"{wan.Timestamp.Date.ToString("yyyy-MM-dd")}; {wan.Timestamp.TimeOfDay}; {wan.IsConnected}; {wan.UpSpeed}; {wan.DownSpeed}";
+            File.AppendAllText(FilePath, line + Environment.NewLine);
+        }
+    }
+}
---
+++ DrayTek WAN Status/WanStatus.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DrayTek_WAN_Status {
+    public class WanStatus {
+        public decimal UpSpeed {
+            get {
+                if (!_upSpeed.HasValue) {
+                    var matchUpSpeed = Regex.Match(_rawContent, @"(?<=UpSpeed=)\d*");
+                    if (!matchUpSpeed.Success) { return 0; }
+                    _upSpeed = Math.Round(Decimal.Parse(matchUpSpeed.Value) / 1024 / 1024, 2);
+                }
+                return _upSpeed.Value;
+            }
+        }
+
+        public decimal DownSpeed {
+            get {
+                if (!_downSpeed.HasValue) {
+                    var matchDownSpeed = Regex.Match(_rawContent, @"(?<=DownSpeed=)\d*");
+                    if (!matchDownSpeed.Success) { return 0; }
+                    _downSpeed = Math.Round(Decimal.Parse(matchDownSpeed.Value) / 1024 / 1024, 2);
+                }
+                return _downSpeed.Value;
+            }
+        }
+
+        public DateTime Timestamp {
+            get {
+                if (!_timestamp.HasValue) {
+                    var matchDate = Regex.Match(_rawContent, "(?<=>).*(?= Vigor)");
+                    if (!matchDate.Success) { return DateTime.MinValue; }
+                    _timestamp = DateTime.ParseExact(matchDate.Value, "MMM  d HH:mm:ss", CultureInfo.InvariantCulture);
+                }
+                return _timestamp.Value;
+            }
+        }
+
+        public bool IsConnected {
+            get {
+                if (!_isConnected.HasValue) {
+                    _isConnected = _rawContent.Contains("States=SHOWTIME");
+                }
+                return _isConnected.Value;
+            }
+        }
+
+        public string SpeedUnit { get { return "Mbps"; } }
+
+        private bool? _isConnected = null;
+        private DateTime? _timestamp = null;
+        private decimal? _downSpeed = null;
+        private decimal? _upSpeed = null;
+
+        private string _rawContent;
+
+
+
+        public static WanStatus Parse(string content) {
+            return ContainsWanStatus(content) ? GetStatus(content) : new WanStatus();
+        }
+
+        public static bool ContainsWanStatus(string content) {
+            return content.Contains("ADSL_Status");
+        }
+
+        private static WanStatus GetStatus(string content) {
+            return new WanStatus {
+                _rawContent = content
+            };
+        }
+    }
+}
